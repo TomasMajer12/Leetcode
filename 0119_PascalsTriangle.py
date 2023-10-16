065710ac35dcf1ee8a564d3734d43a1cdb57f323
TomasMajer12 <tomas.majer12@seznam.cz>
2023-10-16 14:18:54 +0200

    problem 119

---
+++ 0119_PascalsTriangle.py
@@ -0,0 +1,23 @@
+"""
+Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+"""
+
+from typing import List
+
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex == 0:
+            return [1]
+        row = []
+        for i in range(rowIndex):
+            newArray = []
+            newArray.append(1)
+            for i in range(len(row)):
+                if i+1 < len(row):
+                    newArray.append(row[i]+row[i+1])
+            newArray.append(1)
+            row = newArray
+        return row
+    
+print(Solution().getRow(1))
