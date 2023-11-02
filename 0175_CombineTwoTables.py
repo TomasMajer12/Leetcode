9e53a186d1a5d3c56ea0f6ba3c33997477a416fa
TomasMajer12 <tomas.majer12@seznam.cz>
2023-11-02 17:58:34 +0100

    problem 175

---
+++ 0175_CombineTwoTables.py
@@ -0,0 +1,48 @@
+"""
+Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| personId    | int     |
+| lastName    | varchar |
+| firstName   | varchar |
++-------------+---------+
+personId is the primary key (column with unique values) for this table.
+This table contains information about the ID of some persons and their first and last names.
+ 
+
+Table: Address
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| addressId   | int     |
+| personId    | int     |
+| city        | varchar |
+| state       | varchar |
++-------------+---------+
+addressId is the primary key (column with unique values) for this table.
+Each row of this table contains information about the city and state of one person with ID = PersonId.
+ 
+
+Write a solution to report the first name, last name, city, and state of each person in the Person table. If the address of a personId is not present in the Address table, report null instead.
+
+Return the result table in any order.
+
+The result format is in the following example.
+"""
+
+import pandas as pd
+
+def combine_two_tables(person: pd.DataFrame, address: pd.DataFrame) -> pd.DataFrame:
+
+    return pd.merge(person,address, on='personId',how="left")[["firstName", "lastName","city","state"]]
+
+
+
+data = [[1, 'Wang', 'Allen'], [2, 'Alice', 'Bob']]
+person = pd.DataFrame(data, columns=['personId', 'firstName', 'lastName']).astype({'personId':'Int64', 'firstName':'object', 'lastName':'object'})
+data = [[1, 2, 'New York City', 'New York'], [2, 3, 'Leetcode', 'California']]
+address = pd.DataFrame(data, columns=['addressId', 'personId', 'city', 'state']).astype({'addressId':'Int64', 'personId':'Int64', 'city':'object', 'state':'object'})
+print(combine_two_tables(person,address))
