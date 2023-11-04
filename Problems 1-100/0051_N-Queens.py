e9f1dbb7b843a574d4034ff9dbade01066935055
TomasMajer12 <tomas.majer12@seznam.cz>
2023-11-04 10:07:51 +0100

    update

--- Problems 1-100/0051_N-Queens.py
+++ Problems 1-100/0051_N-Queens.py
@@ -13,21 +13,60 @@ from typing import List
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         board = [['.' for _ in range(n)] for _ in range(n)]
-        board[1][1] = 'Q'
-        print(self.rowCheck(board,2))
-        self.printBoard(board)
+        
+        self.getPermutations(0,0,n,board,0)
         return
     
-    def rowCheck(self,board, row):
-        if 'Q' in board[row]:
+    def queenCheck(self,board, row, col,n):
+        if 'Q' in board[row]: #check row
             return False
+        for i in range(n): #check column
+            if board[i][col] == 'Q':
+                return False
+        
+        #check left diagonal up
+        k,l = row,col
+        while k > 0 and l > 0:
+            if board[k][l] == 'Q':
+                return False
+            k-=1
+            l-=1
+        #check left diagonal down
+        k,l = row,col
+        while k < n and l < n:
+            if board[k][l] == 'Q':
+                return False
+            k+=1
+            l+=1
+        #check right diagonal up
+        k,l = row,col
+        while k > 0 and l  < n:
+            if board[k][l] == 'Q':
+                return False
+            k-=1
+            l+=1
+        #check right diagonal down
+        k,l = row,col
+        while k < n and l > 0:
+            if board[k][l] == 'Q':
+                return False
+            k+=1
+            l-=1
         return True
 
 
-    def getPermutations(self,n, board, numberOfQueens):
-
-        for i in range(n):
-            pass
+    def getPermutations(self,startR,startC,n, board, numberOfQueens):
+        myBoard = board
+        for i in range(startR,n):
+            for j in range(startC,n):
+                if numberOfQueens == n:
+                    return self.printBoard(myBoard)
+                if self.queenCheck(board,i,j,n):
+                    myBoard[i][j] = 'Q'
+                    self.printBoard(myBoard)
+                    numberOfQueens+=1
+                    self.getPermutations(i,j,n,myBoard,numberOfQueens)
+                
         return
 
     def printBoard(self,board):
@@ -35,7 +74,7 @@ class Solution:
             for j in range(len(board[0])):
                 print(board[i][j],end="")
             print()
-        return
+        print()
 
 print(Solution().solveNQueens(4))
 
