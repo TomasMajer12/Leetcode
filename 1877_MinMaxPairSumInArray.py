93f6eac410f9fd1d6f489ad55cf02c7c9fa0b0bc
TomasMajer12 <tomas.majer12@seznam.cz>
2023-11-17 19:26:55 +0100

    problem 1877

---
+++ 1877_MinMaxPairSumInArray.py
@@ -0,0 +1,26 @@
+"""
+The pair sum of a pair (a,b) is equal to a + b. 
+The maximum pair sum is the largest pair sum in a list of pairs.
+
+For example, if we have pairs (1,5), (2,3), and (4,4), 
+the maximum pair sum would be max(1+5, 2+3, 4+4) = max(6, 5, 8) = 8.
+Given an array nums of even length n, pair up the elements of nums into n / 2 pairs such that:
+
+Each element of nums is in exactly one pair, and
+The maximum pair sum is minimized.
+Return the minimized maximum pair sum after optimally pairing up the elements.
+"""
+from typing import List
+class Solution:
+    def minPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        maxPair = 0
+        i=0
+        j=len(nums) - 1
+        while i < j:
+            maxPair = max(maxPair,nums[i] + nums[j])
+            i+=1
+            j-=1
+        return maxPair
+
+print(Solution().minPairSum(nums = [3,5,2,3]))
