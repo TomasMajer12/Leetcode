b5f0396c1d1c50e09b408056ab64a65b780af00f
TomasMajer12 <tomas.majer12@seznam.cz>
2024-03-12 18:03:37 +0100

    problem 205

---
+++ 0205.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        return len(set(s))==len(set(t))==len(set(zip(s,t)))
+
+
+
+print(Solution().isIsomorphic("aaeaa","uuxuu"))
