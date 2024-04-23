681cae103b299b99441b25cc5408754296672640
TomasMajer12 <tomas.majer12@seznam.cz>
2024-04-23 19:20:40 +0200

    problem 1661

--- Structures/pandas.py
+++ Structures/pandas.py
@@ -2,3 +2,4 @@
 #sorting => sorted(Dataframe)
 #get to str len => on column .str.len()
 #return in format res[[column1,column2]]
+#mean of different idd --> pd.DataFrame(res.groupby('machine_id')['processing_time'].mean()
