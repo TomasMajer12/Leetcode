1eeecc641a8e1f96357fe52bdb11e2e46738d3e6
TomasMajer12 <tomas.majer12@seznam.cz>
2025-01-05 21:05:57 +0100

    problem 197

---
+++ 0197.py
@@ -0,0 +1,14 @@
+import pandas as pd
+data = [[1, '2000-12-16', 3], [3, '2000-12-15', -1]]
+weather = pd.DataFrame(data, columns=['id', 'recordDate', 'temperature']).astype({'id':'Int64', 'recordDate':'datetime64[ns]', 'temperature':'Int64'})
+
+
+
+def rising_temperature(weather: pd.DataFrame) -> pd.DataFrame:
+    weather.sort_values(by='recordDate', inplace=True)
+    return weather[
+        (weather.temperature.diff() > 0)
+      & (weather.recordDate.diff().dt.days == 1)
+    ][['id']]
+
+print(rising_temperature(weather))
