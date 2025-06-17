1c7cbfbfb6190dd5d814dfa29af931e63ae7fe4c
TomasMajer12 <tomas.majer12@seznam.cz>
2025-06-17 10:42:12 +0200

    problem 182

---
+++ Problems 1-1000/Problems 101-200/0171.py
@@ -0,0 +1,11 @@
+
+
+
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        result = 0
+        for i, char in enumerate(reversed(columnTitle)):
+            result += (ord(char) - ord('A') + 1) * (26 ** i)
+        return result
+    
+print(Solution().titleToNumber("ZY"))  # Output: 701
