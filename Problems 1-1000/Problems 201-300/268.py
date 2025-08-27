328da6715b69732042f063e880bcb24b35c30fbc
TomasMajer12 <tomas.majer12@seznam.cz>
2025-08-27 13:50:50 +0200

    problem 268

---
+++ Problems 1-1000/Problems 201-300/268.py
@@ -0,0 +1,12 @@
+
+from typing import List
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        expected_sum = n * (n + 1) // 2
+        actual_sum = sum(nums)
+        return expected_sum - actual_sum
+
+print(Solution().missingNumber([3,0,1]))
+print(Solution().missingNumber([0,1]))
