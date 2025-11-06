cc1748354f489050ba3893b70d297e7e65b67fb0
TomasMajer12 <tomas.majer12@seznam.cz>
2025-11-06 17:17:26 +0100

    problem 476

---
+++ Problems 1-1000/Problems 401-500/0476.py
@@ -0,0 +1,12 @@
+
+
+class Solution:
+    def findComplement(self, num: int) -> int:
+        if num == 0:
+            return 1
+        
+        bit_length = num.bit_length()
+        mask = (1 << bit_length) - 1
+        return mask ^ num
+
+print(Solution().findComplement(5))
